e182159e4312de2581fed3261a3216610ea18e6c
James Spinella <jspinella@users.noreply.github.com>
2020-05-25 07:45:03 -0400

    whoaa ayyy private API key...

--- Zillow/Constants.cs
+++ Zillow/Constants.cs
@@ -6,7 +6,7 @@ namespace Zillow
 {
     internal static class Constants
     {
-        public const string zwsId = "X1-ZWz1g3flf392q3_5dugt"; //api key
+        public const string zwsId = ""; //api key
         public const string baseUrl = "http://www.zillow.com/webservice/";
         public const string userAgent = "Chanel/0.1";
 
